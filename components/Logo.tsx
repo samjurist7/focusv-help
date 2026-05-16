7d2b1a9ababa8c4796b8e73b5a936fcda9cbe8cb
samjurist7 <250744918+samjurist7@users.noreply.github.com>
2026-05-16 05:53:36 -0400

    fix: show black logo in light mode, white logo in dark mode

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,13 +4,23 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href="/" className="shrink-0">
+      {/* White logo for dark mode */}
       <Image
         src="/focusv-logo.svg"
         alt="Focus V"
         width={120}
         height={24}
         priority
-        className="h-6 w-auto"
+        className="h-6 w-auto dark:block hidden"
+      />
+      {/* Black logo for light mode */}
+      <Image
+        src="/focusv-logo-black.svg"
+        alt="Focus V"
+        width={120}
+        height={24}
+        priority
+        className="h-6 w-auto dark:hidden block"
       />
     </Link>
   );
